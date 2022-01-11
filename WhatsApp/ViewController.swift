2c7e9445daeb392e1beb313ebadf840288684823
Nicolas Famularo <nicolas.famularo@gmail.com>
2022-01-11 16:54:00 -0300

    Initial Commit

---
+++ WhatsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsApp
+//
+//  Created by Nicolas Famularo on 11/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
